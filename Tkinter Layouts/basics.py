32459d8dcc7e12b30fe30f05d5275ce5c6866db5
Viktor Petrov <vikosp01@gmail.com>
2024-03-14 14:50:36 +0200

    menus and working with windows

---
+++ Tkinter Layouts/basics.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+window.geometry('400x600')
+window.title('Grid')
+
+label1 = ttk.Label(master=window, text='Label1', background='red')
+label2 = ttk.Label(master=window, text='Label2', background='blue')
+label3 = ttk.Label(master=window, text='Label3', background='green')
+label4 = ttk.Label(master=window, text='Label4', background='yellow')
+button1 = ttk.Button(master=window, text='A button')
+button2 = ttk.Button(master=window, text='another button')
+entry = ttk.Entry(master=window)
+window.mainloop()
