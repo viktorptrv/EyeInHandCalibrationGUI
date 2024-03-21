a3b0e611dd7da0084d56698d9faddf541d8fc881
Viktor Petrov <vikosp01@gmail.com>
2024-03-21 13:09:26 +0200

    calibration app

--- Automated GUI For Zivid/calibration_app_OOP.py
+++ Automated GUI For Zivid/calibration_app_OOP.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import customtkinter as ctk
 from tkinter import ttk
+from tkinter import messagebox
 from CTkToolTip import *
 from fanucpy import Robot
 # from warmup import warmup
@@ -28,20 +29,8 @@ def get_curposv2(self, uframe, tframe) -> list[float]:
 def connect_to_cam():
     pass
 
-
-def connect_to_robot():
-    global robot_fanuc
-
-    # Initializing the robot
-    robot_fanuc = Robot(
-        robot_model="LHMROB011",
-        host="10.37.115.206",
-        port=18735,
-        ee_DO_type="RDO",
-        ee_DO_num=7,
-    )
-
-    return robot_fanuc
+def init_robot():
+    pass
 
 
 def warmup():
@@ -92,37 +81,55 @@ class TopMenu(ctk.CTkFrame):
         self.button_calibration_auto = None
         self.button_calibration_manual = None
 
+        self.TopMenuFrameOne = RobotFrame(self)
+
         self.place(relx=0.02, rely=0.02, relwidth=0.2, relheight=0.9)
+
+        # self.create_widgets()
+
+    # def create_widgets(self):
+    #     self.button_rob = ctk.CTkButton(master=self,
+    #                                     text='Connect to Robot',
+    #                                     command=connect_to_robot)
+    #     self.button_rob.pack(pady=35, padx=10, side='top')
+    #
+    #     self.button_cam = ctk.CTkButton(master=self,
+    #                                     text='Connect to Camera',
+    #                                     command=connect_to_cam)
+    #     self.button_cam.pack(pady=35, padx=10, side='top')
+    #
+    #     self.button_calibration_auto = ctk.CTkButton(master=self,
+    #                                                  text='Auto Calibration',
+    #                                                  command=auto_calibration)
+    #     self.button_calibration_auto.pack(pady=35, padx=10, side='top')
+    #     CTkToolTip(self.button_calibration_auto, message='For automated calibration you need\n'
+    #                                                      'at least 20 predefined positions of the robot\n'
+    #                                                      'and you must input them in the menu')
+    #
+    #     self.button_calibration_manual = ctk.CTkButton(master=self,
+    #                                                    text='Manual Calibration',
+    #                                                    command=manual_calibration)
+    #     self.button_calibration_manual.pack(pady=10, padx=10, side='top')
+    #     CTkToolTip(self.button_calibration_manual, message='For manual calibration you need\n'
+    #                                                        'at least 20 positions of the robot.\n'
+    #                                                        'Change the position of the robot and press'
+    #                                                        'Get Current Position')
+
+
+class RobotFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.button_rob = None
+        self.button_cam = None
+        self.place(relx=0.1, rely=0.03, relwidth=0.8, relheight=0.3)
 
         self.create_widgets()
 
     def create_widgets(self):
         self.button_rob = ctk.CTkButton(master=self,
                                         text='Connect to Robot',
-                                        command=connect_to_robot)
-        self.button_rob.pack(pady=10, padx=10, side='top')
-
-        self.button_cam = ctk.CTkButton(master=self,
-                                        text='Connect to Camera',
-                                        command=connect_to_cam)
-        self.button_cam.pack(pady=10, padx=10, side='top')
-
-        self.button_calibration_auto = ctk.CTkButton(master=self,
-                                                     text='Auto Calibration',
-                                                     command=auto_calibration)
-        self.button_calibration_auto.pack(pady=10, padx=10, side='top')
-        CTkToolTip(self.button_calibration_auto, message='For automated calibration you need\n'
-                                                         'at least 20 predefined positions of the robot\n'
-                                                         'and you must input them in the menu')
-
-        self.button_calibration_manual = ctk.CTkButton(master=self,
-                                                       text='Manual Calibration',
-                                                       command=manual_calibration)
-        self.button_calibration_manual.pack(pady=10, padx=10, side='top')
-        CTkToolTip(self.button_calibration_manual, message='For manual calibration you need\n'
-                                                           'at least 20 positions of the robot.\n'
-                                                           'Change the position of the robot and press'
-                                                           'Get Current Position')
+                                        command=init_robot)
+        self.button_rob.pack(pady=20, padx=10, side='top')
 
 
 class RightMenuTop(ctk.CTkFrame):
@@ -154,8 +161,6 @@ class MiddleMenuMid(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
         self.place(relx=0.23, rely=0.325, relwidth=0.35, relheight=0.3)
-
-
 
 
 robot_fanuc = None
