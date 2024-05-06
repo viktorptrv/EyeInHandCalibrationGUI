e52b6a8a31f5e1fe6d4c9747beb94a6aa096cfc2
Viktor Petrov <vikosp01@gmail.com>
2024-05-06 19:32:45 +0300

    uni app

---
+++ Auto Robot Calibration/entries.py
@@ -0,0 +1,63 @@
+import customtkinter as ctk
+
+
+class CamIPEntry(ctk.CTkEntry):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.configure(placeholder_text='Camera IP')
+        self.configure(placeholder_text_color='black')
+        self.configure(width=100)
+        # self.configure(fg_color='#ffba33')
+        # self.configure(text_color='black')
+
+
+class CamPortEntry(ctk.CTkEntry):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.configure(placeholder_text='Port')
+        self.configure(placeholder_text_color='black')
+        self.configure(width=60)
+        # self.configure(fg_color='#ffba33')
+        # self.configure(text_color='black')
+
+
+class RobIPEntry(ctk.CTkEntry):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.configure(placeholder_text='Robot IP')
+        self.configure(placeholder_text_color='black')
+        self.configure(width=100)
+        # self.configure(fg_color='#ffba33')
+        # self.configure(text_color='black')
+
+
+class RobPortEntry(ctk.CTkEntry):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.configure(placeholder_text='Port')
+        self.configure(placeholder_text_color='black')
+        self.configure(width=60)
+        # self.configure(fg_color='#ffba33')
+        # self.configure(text_color='black')
+
+
+class RobUFEntry(ctk.CTkEntry):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.configure(placeholder_text='UF')
+        self.configure(placeholder_text_color='black')
+        self.configure(width=40)
+
+
+class RobTFEntry(ctk.CTkEntry):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.configure(placeholder_text='TF')
+        self.configure(placeholder_text_color='black')
+        self.configure(width=40)
--- Auto Robot Calibration/main.py
+++ Auto Robot Calibration/main.py
@@ -17,6 +17,7 @@ from warm_up import WarmUpButton
 from images import ImageCam, ImageRobot, ImageIntr, CamButtonBlur, WarmUpBlur
 from images import CurrPoseBlur, CurrJPoseBlur, SendRobotBlur
 from zivid_image import FrameZivid
+from entries import CamIPEntry, CamPortEntry, RobPortEntry, RobIPEntry, RobTFEntry, RobUFEntry
 
 
 class App(ctk.CTk):
@@ -24,33 +25,57 @@ class App(ctk.CTk):
         super().__init__() # With the super method we include a master
 
         self.title('Calibration App')
-        self.geometry('1400x700')
-        self.maxsize(1400, 700)
-        self.minsize(1400, 700)
+        self.geometry('700x350')
+        # self.maxsize(700, 700)
+        self.minsize(700, 700)
 
         self.robot_button = None
         self.camera_button = None
 
         """
-        Configuring the widgets
+        Configuring the Menus
         """
         self.MenuLeft = FrameLeft(self)
-        self.MenuLeft.place(relx=0.01, rely=0.01, relwidth=0.49, relheight=0.98)
+        self.MenuLeft.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.98)
 
-        self.MenuRight = FrameRight(self)
-        self.MenuRight.place(relx=0.502, rely=0.01, relwidth=0.49, relheight=0.98)
+        # self.MenuRight = FrameRight(self)
+        # self.MenuRight.place(relx=0.502, rely=0.01, relwidth=0.49, relheight=0.98)
 
+        """
+        Configuring Entries
+        """
+        self.CamIPEntry = CamIPEntry(self.MenuLeft)
+        self.CamIPEntry.place(relx=0.05, rely=0.05)
+
+        self.CamPortEntry = CamPortEntry(self.MenuLeft)
+        self.CamPortEntry.place(relx=0.20, rely=0.05)
+
+        self.RobIPEntry = RobIPEntry(self.MenuLeft)
+        self.RobIPEntry.place(relx=0.55, rely=0.05)
+
+        self.RobPortEntry = RobPortEntry(self.MenuLeft)
+        self.RobPortEntry.place(relx=0.70, rely=0.05)
+
+        self.RobUF = RobUFEntry(self.MenuLeft)
+        self.RobUF.place(relx=0.55, rely=0.005)
+
+        self.RobTF = RobTFEntry(self.MenuLeft)
+        self.RobTF.place(relx=0.612, rely=0.005)
+
+        """
+        Configuring Buttons
+        """
         self.RobotButton = RobotButton(self.MenuLeft)
         self.RobotButton.place(relx=0.55, rely=0.1)
 
         self.CameraButton = CameraButton(self.MenuLeft)
         self.CameraButton.place(relx=0.05, rely=0.1)
 
-        self.CalibrateButton = CalibrateButton(self.MenuRight)
-        self.CalibrateButton.place(relx=0.35, rely=0.1)
+        self.CalibrateButton = CalibrateButton(self.MenuLeft)
+        self.CalibrateButton.place(relx=0.35, rely=0.90)
 
-        self.FrameZivid = FrameZivid(self.MenuRight)
-        self.FrameZivid.place(relx=0.01, rely=0.2, relwidth=0.98, relheight=0.79)
+        # self.FrameZivid = FrameZivid(self.MenuRight)
+        # self.FrameZivid.place(relx=0.01, rely=0.2, relwidth=0.98, relheight=0.79)
 
         self.WarmUp = WarmUpButton(self.MenuLeft)
         self.WarmUp.place(relx=0.15, rely=0.2)
@@ -103,6 +128,9 @@ class App(ctk.CTk):
         self.SendRobotBlur = SendRobotBlur(self.MenuLeft)
         self.SendRobotBlur.place(relx=0.65, rely=0.4)
 
+        """
+        За функциолността на бутоните, ще ги включим тук в отделна функция за всеки бутон
+        """
         self.CameraButton.configure(command=lambda: self.WarmUpBlur.place_forget())
 
 
